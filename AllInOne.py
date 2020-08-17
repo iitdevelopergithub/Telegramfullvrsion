e5d059870ebd9fae043cc01dcb7592f8aa089c9d
IIT DEVELOPER <67006291+iitdevelopergithub@users.noreply.github.com>
2020-08-17 15:28:49 +0530

    Update AllInOne.py

--- AllInOne.py
+++ AllInOne.py
@@ -1,5 +1,4 @@
 import configparser
-import banner as 
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
