443c7e90b73242c7824692f6821cd53933466b01
IIT DEVELOPER <67006291+iitdevelopergithub@users.noreply.github.com>
2020-08-17 15:19:14 +0530

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,25 @@
+import Telegram2.O
+import os, sys
+re="\033[1;31m"
+gr="\033[1;32m"
+cy="\033[1;36m"
+
+def Install_banner():
+def Name_banner():
+    os.system('clear')
+	print(f"""
+   (gr+" ******Welcome to IIT DEVELOPER *******")
+      	{re}╦ ╦ ╔╦╗{cy}╔═╗ ┌─┐ ┬ ┬ ╔═╗ ║  ╔═╗ ┌─┐┌─┐┬─┐
+        {re}║ ║  ║ {cy}║ ║ ├┤  │ │ ├┤  ║  ║ ║ ├─┘├┤ ├┬┘
+        {re}╩ ╩  ╩ {cy}╚═╝ └─┘  ─  ╚═╝ ╚═ ╚═╝ ┴  └─┘┴└─
+        version : 2.0
+        {re} Created By {cy} IIT DEVELOPER {gr}Team 
+        """)
+        
+    
+def setup_banner():
+     Name_banner()
+	{re}╔═╗{cy}┌─┐┌┬┐┬ ┬┌─┐
+	{re}╚═╗{cy}├┤  │ │ │├─┘
+	{re}╚═╝{cy}└─┘ ┴ └─┘┴
+	""")     
