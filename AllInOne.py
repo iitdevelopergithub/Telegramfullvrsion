77a320b395853e7ed7d2943e8c4fc5320d9e9886
IIT DEVELOPER <67006291+iitdevelopergithub@users.noreply.github.com>
2020-08-22 20:04:44 +0530

    Update AllInOne.py

--- AllInOne.py
+++ AllInOne.py
@@ -4,6 +4,7 @@ from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 from telethon.tl.functions.channels import InviteToChannelRequest
+import telethon
 import sys
 import csv
 import traceback
